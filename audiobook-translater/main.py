e6109d1939df2913adc18a1990bee1d9487c4be0
Diogo Sperandio <76974801+dsperax@users.noreply.github.com>
2023-10-25 23:10:59 -0300

    atualizando main.py para versão atual das libs

--- audiobook-translater/main.py
+++ audiobook-translater/main.py
@@ -1,6 +1,7 @@
 import pyttsx3
 import pdfplumber
 import PyPDF2
+from deep_translator import GoogleTranslator
 from googletrans import Translator
 
 file = 'LePetitPrince.pdf'
@@ -10,11 +11,16 @@ pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 pages = pdfReader.numPages
 translator = Translator()
 with pdfplumber.open(file) as pdf:
-    for i in range(12, pages):
+    for i in range(4, pages):
         page = pdf.pages[i]
         text = page.extract_text()
-        translation = translator.translate(text, dest='pt')
-        print(translation.text)
-        speaker = pyttsx3.init()
-        speaker.say(translation)
-        speaker.runAndWait()
+        # Verifica se o texto não está vazio ou None
+        if text and text.strip():
+            try:
+                translation = GoogleTranslator(source='auto', target='pt').translate(text)
+                print(translation)
+                speaker = pyttsx3.init()
+                speaker.say(translation)
+                speaker.runAndWait()
+            except Exception as e:
+                print(f"Erro durante a tradução: {e}")
